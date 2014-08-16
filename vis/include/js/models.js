c2de2bad1e9201e8ec8fd1a9b60da23687a62bab
mjasher <michael.james.asher@gmail.com>
2014-08-16 15:22:14 +1000

    include

---
+++ vis/include/js/models.js
@@ -0,0 +1,15 @@
+var Event = Backbone.Model.extend(){
+
+	
+}
+
+// Add an event 
+// name and description
+// tags 
+// click on map or type in address
+// search in calender or type in address
+// invite ?
+
+// find an event
+// pick point and distance (default is bbox of screen)
+// view calender
