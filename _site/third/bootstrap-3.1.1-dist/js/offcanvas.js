59be717b9431d5056fd372ddcbb433c69f4319d1
mjasher <michael.james.asher@gmail.com>
2015-03-25 16:56:32 +1100

    cv update

--- _site/third/bootstrap-3.1.1-dist/js/offcanvas.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $('[data-toggle=offcanvas]').click(function () {
-    $('.row-offcanvas').toggleClass('active')
-  });
-    
-('[data-toggle=leftOffcanvas]').click(function () {
-    $('.row-offcanvas').toggleClass('active')
-  });    
-});
---
+++ blog/blog-template.md
@@ -0,0 +1,9 @@
+
+
+## Minimalist slide out menu
+
+
+
+[alijafarian](http://alijafarian.com/jquery-horizontal-slideout-menu/)
+ [js library](https://mango.github.io/slideout/)
+[fancy](http://tympanus.net/Development/OffCanvasMenuEffects/sideslide.html)
